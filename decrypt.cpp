8865b3f60a1682b605cc7ecbec9995719cb1422d
Sayash <58775812+sayashraaj@users.noreply.github.com>
2020-02-27 21:32:12 +0530

    Update

--- decrypt.cpp
+++ decrypt.cpp
@@ -5,5 +5,13 @@ int main()
 {
 	string s;
 	cin>>s;
-	
+	int n=s.length();
+	int key=int(s[n-1])*10+int(s[n-2]);
+	int arr[n-2];
+	cout<<"The Decrypted Message Is: /n";
+	for(int i=0;i<n-2;i++)
+	{
+		arr[i]=int(s[i])+key;
+		cout<<arr[i];
+	}
 }
